d40f251e8e6c031fe304828b9e3e6c4bf5e9c3a2
mohammadahsankoderlabs <72415016+mohammadahsankoderlabs@users.noreply.github.com>
2020-10-06 01:28:23 +0500

    Create push-airship.js

---
+++ push-airship.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1601923827.4894443
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://www.koderlabs.com/assets/img/logo/new\u002Dlogo.png',
+  defaultTitle: 'asdasdasd',
+  defaultActionURL: 'https://www.koderlabs.com/',
+  appKey: 'jRJCfBp7QteCDXjN3m51jA',
+  token: 'MTpqUkpDZkJwN1F0ZUNEWGpOM201MWpBOjdDdnZkRmk0QTE0TFNBSFJneVFadzR3TnNXX2xIMXVadnUxaWZUSHdKTmc',
+  vapidPublicKey: 'BJK-wKCSsyYYkoBWWT-UMxLixmJZ-PVe11Tidxd3KGqhIJYpSA9jkDASk25pHjy69lvsIaxbNndbwse_BV0oZi8='
+})
