5d1e14173b99e6df29e30f597bc59d5713d171cc
mohammadahsankoderlabs <72415016+mohammadahsankoderlabs@users.noreply.github.com>
2020-10-06 17:24:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,3 @@ echo
 ?>
 
 
-<script type="text/javascript">
-!function(n,t,c,e,u){function r(n){try{f=n(u)}catch(n){return h=n,void i(p,n)}i(s,f)}function i(n,t){for(var c=0;c<n.length;c++)d(n[c],t);
-}function o(n,t){return n&&(f?d(n,f):s.push(n)),t&&(h?d(t,h):p.push(t)),l}function a(n){return o(!1,n)}function d(t,c){
-n.setTimeout(function(){t(c)},0)}var f,h,s=[],p=[],l={then:o,catch:a,_setup:r};n[e]=l;var v=t.createElement("script");
-v.src=c,v.async=!0,v.id="_uasdk",v.rel=e,t.head.appendChild(v)}(window,document,'https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js',
-  'UA', // This value can be changed to use a custom variable name.
-  {
-  appKey: 'jRJCfBp7QteCDXjN3m51jA',
-  token: 'MTpqUkpDZkJwN1F0ZUNEWGpOM201MWpBOjdDdnZkRmk0QTE0TFNBSFJneVFadzR3TnNXX2xIMXVadnUxaWZUSHdKTmc',
-
-  // Safari Web Push only
-  websitePushId: 'BJK-wKCSsyYYkoBWWT-UMxLixmJZ-PVe11Tidxd3KGqhIJYpSA9jkDASk25pHjy69lvsIaxbNndbwse_BV0oZi8=',
-  // Open Web Push Protocol only.
-  vapidPublicKey: 'BJK-wKCSsyYYkoBWWT-UMxLixmJZ-PVe11Tidxd3KGqhIJYpSA9jkDASk25pHjy69lvsIaxbNndbwse_BV0oZi8=',
-  // Only needed when used on insecure hosts:
-  secureIframeUrl: 'https://your.secure.domain/path/to/web-push-secure-bridge.html'
-  })
-</script>
-
